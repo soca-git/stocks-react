aa4856486c188ef6ba34d27d7132152f74c1794f
soca-git <soca.mailbox@gmail.com>
2022-10-18 22:50:43 +0100

    conditionally render color of price delta

--- src/Components/StockPrice/stockprice.js
+++ src/Components/StockPrice/stockprice.js
@@ -2,11 +2,13 @@ import './stockprice.css';
 
 function StockPrice(props)
 {
+    var deltaClassName = props.activeStockPreview?.currentDelta >= 0 ? "stock-price-delta-positive" : "stock-price-delta-negative";
+
     return(
-    <div className="component stock-price">
-        <h3 className="stock-price-current">{props.activeStockPreview?.currentPrice}</h3>
-        <h3 className="stock-price-delta">{props.activeStockPreview?.currentDelta}</h3>
-    </div>
+        <div className="component stock-price">
+            <h3 className="stock-price-current">{props.activeStockPreview?.currentPrice}</h3>
+            <h3 className={deltaClassName}>{props.activeStockPreview?.currentDelta}</h3>
+        </div>
     );
 }
 
