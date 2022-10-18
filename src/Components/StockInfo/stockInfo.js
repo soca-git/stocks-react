07bef87f4789cddc95da948e5c30931dbe8bb70f
soca-git <soca.mailbox@gmail.com>
2022-10-18 15:27:46 +0100

    update css -> add component

--- src/Components/StockInfo/stockInfo.js
+++ src/Components/StockInfo/stockInfo.js
@@ -3,7 +3,7 @@ import './stockinfo.css';
 function StockInfo(props)
 {
     return(
-        <div className="stock-info">
+        <div className="component stock-info">
             <div className="stock-info-title">
                 <h2>{props.activeStock.TickerSymbol}</h2>
                 <h3>{props.activeStock.Name}</h3>
