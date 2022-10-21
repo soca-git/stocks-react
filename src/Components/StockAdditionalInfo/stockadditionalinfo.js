59538fe628f0f2cc1fcd786d523d82c46835e8c5
soca-git <soca.mailbox@gmail.com>
2022-10-21 21:08:09 +0100

    update handling null data, set min widths

--- src/Components/StockAdditionalInfo/stockadditionalinfo.js
+++ src/Components/StockAdditionalInfo/stockadditionalinfo.js
@@ -8,24 +8,24 @@ function StockAdditionalInfo(props)
             <table>
                 <tbody>
                     <tcol>
-                        <div><th>Open</th><td>{props.activeStockAdvancedQuote?.openPrice}</td></div>
-                        <div><th>High</th><td>{props.activeStockAdvancedQuote?.highPrice}</td></div>
-                        <div><th>Low</th><td>{props.activeStockAdvancedQuote?.lowPrice}</td></div>
+                        <div><th>Open</th><td>{props.activeStockAdvancedQuote?.openPrice ?? "-"}</td></div>
+                        <div><th>High</th><td>{props.activeStockAdvancedQuote?.highPrice ?? "-"}</td></div>
+                        <div><th>Low</th><td>{props.activeStockAdvancedQuote?.lowPrice ?? "-"}</td></div>
                     </tcol>
                     <tcol>
-                        <div><th>Vol</th><td>{FormatVolume(props.activeStockAdvancedQuote?.volume)}</td></div>
-                        <div><th>P/E</th><td>{props.activeStockAdvancedQuote?.pricePerEarningsRatio}</td></div>
-                        <div><th>Mkt Cap</th><td>{FormatMarketCap(props.activeStockAdvancedQuote?.marketCap)}</td></div>
+                        <div><th>Vol</th><td>{FormatVolume(props.activeStockAdvancedQuote?.volume) ?? "-"}</td></div>
+                        <div><th>P/E</th><td>{props.activeStockAdvancedQuote?.pricePerEarningsRatio ?? "-"}</td></div>
+                        <div><th>Mkt Cap</th><td>{FormatMarketCap(props.activeStockAdvancedQuote?.marketCap ?? "-")}</td></div>
                     </tcol>
                     <tcol>
-                        <div><th>52W H</th><td>{props.activeStockAdvancedQuote?.fiftyTwoWeekHigh}</td></div>
-                        <div><th>52W L</th><td>{props.activeStockAdvancedQuote?.fiftyTwoWeekLow}</td></div>
-                        <div><th>Avg Vol</th><td>{FormatVolume(props.activeStockAdvancedQuote?.averageVolume)}</td></div>
+                        <div><th>52W H</th><td>{props.activeStockAdvancedQuote?.fiftyTwoWeekHigh ?? "-"}</td></div>
+                        <div><th>52W L</th><td>{props.activeStockAdvancedQuote?.fiftyTwoWeekLow ?? "-"}</td></div>
+                        <div><th>Avg Vol</th><td>{FormatVolume(props.activeStockAdvancedQuote?.averageVolume) ?? "-"}</td></div>
                     </tcol>
                     <tcol>
-                        <div><th>Yield</th><td>{props.activeStockAdvancedQuote?.yield}</td></div>
-                        <div><th>Beta</th><td>{props.activeStockAdvancedQuote?.beta}</td></div>
-                        <div><th>EPS</th><td>{props.activeStockAdvancedQuote?.earningsPerShare}</td></div>
+                        <div><th>Yield</th><td>{props.activeStockAdvancedQuote?.yield ?? "-"}</td></div>
+                        <div><th>Beta</th><td>{props.activeStockAdvancedQuote?.beta ?? "-"}</td></div>
+                        <div><th>EPS</th><td>{props.activeStockAdvancedQuote?.earningsPerShare ?? "-"}</td></div>
                     </tcol>
                 </tbody>
             </table>
